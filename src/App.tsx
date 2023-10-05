c2ac6b681662b5ac597dec4b885d99798aca9680
Dmitrii Sobin <sobinda@srgroup.ru>
2023-10-05 17:15:15 +0300

    Написание отпправки

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
 
 
 const App: FC = () => {
+    const [todos, setTodos] = useState<string[]>([]);
+    const addHandler = (title: string) => {
+        console.log('add new todo', title);
+    };
+
     return <>
         <Navbar/>
         <div className="container">
-            <TodoForm/>
+            <TodoForm onAdd={addHandler}/>
 
 
         </div>
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,13 +1,49 @@
-import React, {FC} from "react";
+import React, {ChangeEvent, FC, FormEvent, useState} from "react";
+import {Simulate} from "react-dom/test-utils";
+import keyPress = Simulate.keyPress;
 
-export const TodoForm:FC = () => {
+interface TodoFormProps {
+    onAdd(title: string): void
+}
+
+
+export const TodoForm: FC<TodoFormProps> = (props) => {
+    const [title, setTitle] = useState<string>('');
+
+    const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.target.value);
+    }
+
+    //todo Требуется разобраться, так как dep метод для onKeyPress
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        props.onAdd(title)
+        setTitle('');
+    }
+
+    // const keyPressHandler = (event: KeyboardEvent) => {
+    //     if (event.key === 'Enter') {
+    //         console.log('Нажали enter')
+    //     }
+    // }
+
     return (
-        <div className="input-field mt2">
-            <input type="text" id='title' placeholder='Введите название дела'/>
-            <label htmlFor="title" className='active'>
-                Введите название дела
-            </label>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div className="input-field mt2">
+                <input
+                    onChange={changeHandler}
+                    value={title}
+                    type="text"
+                    id='title'
+                    placeholder='Введите название дела'
+                    // onKeyPress={keyPressHandler} //dep
+                />
+                <label htmlFor="title" className='active'>
+                    Введите название дела
+                </label>
+            </div>
+        </form>
+
     );
 }
 
