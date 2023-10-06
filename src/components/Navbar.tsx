25f758c2272a4a53d82b6dc113b31a9d1575f8b9
SobinDA <SobinDA@srgroup.ru>
2023-10-06 13:41:13 +0300

    fix button and link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar: FC = () => {
     return (
         <nav>
             <div className="nav-wrapper purple darken-2 px1">
-                <a href="/" className="brand-logo">React + TypeScript</a>
+                <NavLink to="/" className="brand-logo">React + TypeScript</NavLink>
                 <ul className="right hide-on-med-and-down">
                     <li><NavLink to="/">Список дел</NavLink></li>
                     <li><NavLink to="/about">Информация</NavLink></li>
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -8,7 +8,7 @@ export const AboutPage: FC = () => {
             <h1>Страница информации </h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deserunt ipsa maiores
                 quae. Minus non pariatur quisquam, sunt ullam voluptatibus.</p>
-            <button className='bnt' onClick={() => history.push('/')}>Обратно к списку дел</button>
+            <button className='btn' onClick={() => history.push('/')}>Обратно к списку дел</button>
         </>
     );
 }
